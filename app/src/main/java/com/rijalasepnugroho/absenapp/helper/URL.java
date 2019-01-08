af6aa19e806bf89b389aee7c9ff098d433f86f09
rijal rebelworks <rijal.asep@rebelworks.co>
2019-01-08 17:43:21 +0700

    change api url

--- app/src/main/java/com/rijalasepnugroho/absenapp/helper/URL.java
+++ app/src/main/java/com/rijalasepnugroho/absenapp/helper/URL.java
@@ -1,7 +1,7 @@
 package com.rijalasepnugroho.absenapp.helper;
 
 public class URL {
-    public static final String BASE_API = "http://45.77.171.151/api/";
+    public static final String BASE_API = "http://45.77.47.52/api/";
 
     public static final String GET_TOKEN = BASE_API + "api_login.php";
     public static final String ABSEN = BASE_API + "api_attendance.php";
