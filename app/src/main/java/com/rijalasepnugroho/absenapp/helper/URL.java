6bff0481657f8f23d2ddde7d77890446cef99f24
rijal rebelworks <rijal.asep@rebelworks.co>
2019-02-08 10:54:37 +0700

    change api url and bugfix text tasti terpotong

--- app/src/main/java/com/rijalasepnugroho/absenapp/helper/URL.java
+++ app/src/main/java/com/rijalasepnugroho/absenapp/helper/URL.java
@@ -1,7 +1,7 @@
 package com.rijalasepnugroho.absenapp.helper;
 
 public class URL {
-    public static final String BASE_API = "http://45.77.47.52/api/";
+    public static final String BASE_API = "https://tastiapp.southeastasia.cloudapp.azure.com/api/";
 
     public static final String GET_TOKEN = BASE_API + "api_login.php";
     public static final String ABSEN = BASE_API + "api_attendance.php";
